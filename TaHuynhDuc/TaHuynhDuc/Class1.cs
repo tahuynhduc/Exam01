2b1449943c4d0737e739eb1225b3d38ac0298dab
tahuynhduc <tahduc03032000@gmail.com>
2023-03-21 09:02:59 +0700

    Exam1

---
+++ TaHuynhDuc/TaHuynhDuc/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaHuynhDuc
+{
+    public class Student
+    {
+        public int Id;
+        public string Name;
+        public string Gender;
+        public int Age;
+        public DateTime DateOfBirth { get; set; }
+        public int Class;
+        public float Mark1;
+        public float Mark2;
+        public float Mark3;
+
+        public Student(int id, string name , string gender, int age, int clas ,float mark1,float mark2,float mark3)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Gender = gender;
+            this.Age = age;
+            this.Mark1 = mark1;
+            this.Mark2 = mark2;
+            this.Mark3 = mark3;
+        }
+        public float mark_AVG()
+        {
+            return (Mark1 + Mark2 + Mark3) / 3;
+        }
+    }
+    public class TestStudent
+    {
+        public int id { get; set; }
+        public string name{ get; set; }
+        public void Student()
+        {
+            Console.WriteLine($"input ID");
+            Console.ReadLine(id);
+            Console.WriteLine($"input name");
+            Console.ReadLine(name);
+            Console.WriteLine($"input gender");
+            Console.ReadLine();
+            Console.WriteLine($"input date of birth");
+            Console.ReadLine();
+            Console.WriteLine($"input class");
+            Console.ReadLine();
+        }
+        public void SearchStudent()
+        {
+            Console.WriteLine("Input name of student you want search:");
+            Console.ReadLine();
+        }
+        
+    }
+        
+}
---
+++ TaHuynhDuc/TaHuynhDuc/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using System.Security.Claims;
+using System.Xml.Linq;
+using TaHuynhDuc;
+
+namespace kt
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            //Student st = new Student(1, "test1", "male", 12, 12, 5, 6, 7);
+            //Console.WriteLine("input mark 1");
+            //st.Mark1 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("input mark 2");
+            //st.Mark2 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("input mark 3");
+            //st.Mark3 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("input ID");
+            //st.Id = int.Parse(Console.ReadLine());
+            //Console.WriteLine("input Name");
+            //st.Name = Console.ReadLine();
+            //Console.WriteLine("input Gender");
+            //st.Gender = Console.ReadLine();
+            //Console.WriteLine("input Date of birth");
+            //st.DateOfBirth = Convert.ToDateTime(Console.ReadLine());
+
+            //if (st.Mark1 > 0 && st.Mark2 > 0 && st.Mark3 > 0 && st.Mark1 < 100 && st.Mark2 < 100 && st.Mark3 < 100)
+            //{
+            //    Console.WriteLine(st.mark_AVG());
+            //}
+            //else { Console.WriteLine("nhap sai"); }
+
+            TestStudent test = new TestStudent();
+
+            Console.WriteLine(test.Student);
+
+
+
+
+
+
+            Console.Read();
+        }
+    }
+}
